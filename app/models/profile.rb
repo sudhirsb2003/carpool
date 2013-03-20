12b81382651b9c90d88380a0839a4ab6ae1722c1
sudhir <sudhir@sudhir-Satellite-A200.(none)>
2013-03-20 19:12:25 +0530

    profile fields updated

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,12 @@ class Profile < ActiveRecord::Base
   attr_accessible :destination_location, :fisrt_name, :job_title, :last_name, :source_location,
                   :user_id, :photo, :profile_title, :vehicle_type, :accomodation_for
   #validates_presence_of :profile_title, :message =>"profile title can not be empty !"
+  validates :title, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
+  validates :budget, :presence => true, :length => { :within => 1..10000000 }
 
 
-  VEHICLE_TYPE  = %w[Car Auto-Rikshaw Mini-Bus Bus Jeep 8-Seater]
+
+  VEHICLE_TYPE  = %w[Car Auto-Rikshaw Mini-Bus Bus Jeep 8-Seater Two-Wheeler]
 
   has_attached_file :photo,
      :styles => {
